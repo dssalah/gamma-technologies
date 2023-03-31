7a5edcedda1b88cfebc8de64ec9608f53b35fde9
Durran Salah <dosalah23@gmail.comm>
2023-03-31 16:13:57 -0700

    dyanmic progress bar

--- assets/src/main.js
+++ assets/src/main.js
@@ -235,3 +235,19 @@ setTimeout(() => {
  
  */
 
+ function startTimer() {
+    var startTime = Date.now();
+    setInterval(function() {
+      var elapsedTime = Date.now() - startTime;
+      var hours = Math.floor(elapsedTime / (1000 * 60 * 60));
+      var minutes = Math.floor((elapsedTime % (1000 * 60 * 60)) / (1000 * 60));
+      var seconds = Math.floor((elapsedTime % (1000 * 60)) / 1000);
+      var timerElement = document.getElementById('timer');
+      timerElement.textContent = padZero(hours) + ':' + padZero(minutes) + ':' + padZero(seconds);
+    }, 1000);
+  }
+  
+  function padZero(num) {
+    return (num < 10 ? '0' : '') + num;
+  }
+  
